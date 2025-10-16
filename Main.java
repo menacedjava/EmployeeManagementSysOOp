39448fb3b83ddda5adf8554e52a708eed9af841e
menacedjava <menacedjava@users.noreply.github.com>
2025-10-16 16:34:08 +0500

    Final manga project

---
+++ Main.java
@@ -0,0 +1,9 @@
+Команда AT признана нерекомендуемой. Используйте вместо нее schtasks.exe.
+
+Такой запрос не поддерживается.
+// Character system added
+public class Character {
+    String name;
+    int level;
+}
+// Character system added
